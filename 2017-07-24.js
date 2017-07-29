9648c52d875ad2bc172e7e10de80b49a99fe7146
zack <zengyangzhao@outlook.com>
2017-07-29 12:27:50 +0800

    project

--- 2017-07-24.js
+++ 2017-07-24.js
@@ -3,11 +3,14 @@ var childprocess = require('child_process');
 var webdriver = require("selenium-webdriver")
 var By = webdriver.By;
 var driver = new webdriver.Builder().forBrowser('chrome').build();
-
+var path = require('path');
 driver.get('http://www.baidu.com/')
 driver.sleep(3000);
 driver.findElement(By.css('.soutu-btn')).click();
-// driver.findElement(By.css('.upload-pic')).sendKeys("D:\\tranning\\-\\includefile\\gherkin.png");
-driver.findElement(By.css('.upload-pic')).click().then(()=>{
-    childprocess.execFileSync('./includefile/uploadfile.exe');
-})
+let filepath = path.join(__dirname,'includefile','gherkin.png');
+console.log(filepath)
+driver.findElement(By.css('.upload-pic')).sendKeys(filepath);
+
+// driver.findElement(By.css('.upload-pic')).click().then(()=>{
+//     childprocess.execFileSync('./includefile/uploadfile.exe');
+// })
--- readme.md
+++ readme.md
@@ -4,6 +4,12 @@
 |--|--|
 2017-07-18 | JavaScript 回调
 2017-07-20 | JavaScript Promise
-2017-07-21 | Node.js moudle 
- 
-
+2017-07-21 | Node.js moudle exports,require
+2017-07-22 | JavaScript 对象
+2017-07-23 | 部署linux 环境 使用最简单的方式做nodeclub社区的自动化测试
+2017-07-24 | selenium-webdriver sendKeys() 上传文件或者使用 AutoIt 辅助上传 
+2017-07-25 | Node.js Path
+2017-07-26 | Node.js Child Processes
+2017-07-27 | Node.js fs
+2017-07-28 | selenium-webdriver Keys快捷键的操作
+2017-07-29 | selenium-webdriver 创建手机模拟器
